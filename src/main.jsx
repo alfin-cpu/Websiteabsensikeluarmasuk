3367c0c4a9da2fb07ba889c37ed64f28817c9655
alfin-cpu <alfinas090999@gmail.com>
2025-11-20 22:13:29 +0700

    Clarify CSS import in main.jsx
    Added a comment to clarify the CSS import.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // Pastikan ini mengarah ke file CSS global Anda
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
